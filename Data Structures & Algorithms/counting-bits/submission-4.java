49fe3b3c9b6978f22677e842157ff599a6af0699
MinionHub06 <om.nayakkatni@gmail.com>
2026-07-18 11:34:59 +0530

    Add: counting-bits - submission-4

---
+++ Data Structures & Algorithms/counting-bits/submission-4.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int[] countBits(int n) {
+        int[] out=new int[n+1];
+        out[0]=0;
+        for(int i=1;i<=n;i++){
+            int x=i;
+            int res=0;
+            while(x!=0){
+                res+=(x&1)==1?1:0;
+                x>>=1;
+            }
+            out[i]=res;
+        }
+        return out;
+    }
+}
